5dc8849c3eaa18a0b0d348b36da849662b4c78c0
Артём Кириенко <art-kirienko@ya.ru>
2012-12-03 22:47:28 +0400

    Initial Commit

---
+++ v21-5/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  v21-5
+//
+//  Created by Артём Кириенко on 03.12.12.
+//  Copyright (c) 2012 Артём Кириенко. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
